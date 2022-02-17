eae5b696dd6c9ad3f11b8376e29dddb07dbd1630
mkhy19 <mkhy88519@gmail.com>
2022-02-17 12:13:11 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import Navbar from './compontents/Navbar';
 import Home from './compontents/Home';
-// import Products from './compontents/Products';
-
+import Products from './compontents/Products';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-
+ 
 class App extends Component{
 
   render(){
     return (
       <div className="App">
         <Navbar />
-        <Home />
-        {/* <Products /> */}
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/products' element={<Products />} />
+        </Routes>
+
       </div>
     );
   }
